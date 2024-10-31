12af88f07381ba3df3c619a488ad8351c78f306d
luella <sun.luella@gmail.com>
2024-10-31 12:16:54 -0400

    Update MotivationalMessage.swift

--- Views/MotivationalMessage.swift
+++ Views/MotivationalMessage.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct MotivationalMessage: View {
     private let messages = [
         "wait until you're really hungry and then eat until you're really full",
-        "stop thinking about food and go make some art",
-        "eating too many carbs will give you yeast infections",
+        "stop thinking about food go do something else",
+        "eating too many carbs will give you a yeast infection",
         "you can eat at a 40% calorie deficit and still maintain muscle mass if you eat 2.2g of protein per kg lean body mass",
         "eat soup",
         "take your supplements"
@@ -27,7 +27,7 @@ struct MotivationalMessage: View {
             .padding(.horizontal)
             .onTapGesture {
                 withAnimation {
-                    currentMessageIndex = Int.random(in: 0..<messages.count)
+                    currentMessageIndex = (currentMessageIndex + 1) % messages.count
                 }
             }
     }
